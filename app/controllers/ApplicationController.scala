1cc1946b08ea7382fae9e1c4040cb26d93957735
Hubert Świetlicki <hswietlicki@systherm-info.pl>
2017-01-10 15:17:18 +0100

    Działa stare logowanie.

---
+++ app/controllers/ApplicationController.scala
@@ -0,0 +1,39 @@
+package controllers
+
+import java.io.ByteArrayOutputStream
+import java.net.URLEncoder
+import java.util.zip.{Deflater, DeflaterOutputStream}
+
+import com.google.inject.Inject
+import models.SoapRequester
+import org.apache.commons.codec.binary.Base64
+import play.api.Configuration
+import play.api.mvc.{Action, Controller}
+
+class ApplicationController @Inject() (configuration: Configuration) extends Controller {
+
+  def index = Action { request =>
+    request.session.get("EPUAP_SESSION_ID").fold(
+      Redirect(routes.ApplicationController.sendAuthnRequest())
+    )(
+      sessionId => Ok("Logged in via EPUAP with sessionId=" + sessionId)
+    )
+  }
+
+  def sendAuthnRequest = Action { request =>
+    val baos = new ByteArrayOutputStream()
+    val dos = new DeflaterOutputStream(baos, new Deflater(Deflater.DEFAULT_COMPRESSION, true))
+    dos.write(views.xml.authnRequest.render().toString().getBytes("UTF-8"))
+    dos.close()
+    baos.close()
+    Redirect("https://hetman-int.epuap.gov.pl/DracoEngine2/draco.jsf?SAMLRequest=" + URLEncoder.encode(new String(Base64.encodeBase64(baos.toByteArray)), "UTF-8"))
+  }
+
+  def resolveArtifact(SAMLart: Option[String]) = Action { request =>
+    val nodes = xml.XML.loadString(SoapRequester.resolveArtifact(views.xml.artifactResolve.render(SAMLart).toString()))
+    val sessionId = (nodes \ "Body" \ "ArtifactResponse" \ "Response" \ "Assertion").head.attribute("ID").head.text
+    Redirect(routes.ApplicationController.index()).withSession(
+      "EPUAP_SESSION_ID" -> sessionId
+    )
+  }
+}
---
+++ app/models/SoapRequester.scala
@@ -0,0 +1,35 @@
+package models
+
+import java.io.{ByteArrayOutputStream, StringReader}
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.soap.{MessageFactory, SOAPConnectionFactory, SOAPMessage}
+
+import org.w3c.dom.Document
+import org.xml.sax.InputSource
+
+object SoapRequester {
+
+  private val connection = SOAPConnectionFactory.newInstance.createConnection()
+
+  def resolveArtifact(body: String): String = {
+    val response = connection.call(buildMessage(body), "https://konsolahetman-int.epuap.gov.pl/axis2/services/EngineSAMLArtifact")
+    val baos = new ByteArrayOutputStream
+    response.writeTo(baos)
+    baos.close()
+    new String(baos.toByteArray, "UTF-8")
+  }
+
+  private def buildMessage(body: String): SOAPMessage = {
+    val request = MessageFactory.newInstance.createMessage()
+    request.getSOAPBody.addDocument(buildDocument(body))
+    request.getMimeHeaders.addHeader("SOAPAction", "https://konsolahetman-int.epuap.gov.pl/axis2/services/EngineSAMLArtifact/artifactResolve")
+    request.saveChanges()
+    request
+  }
+
+  private def buildDocument(body: String): Document = {
+    val factory = DocumentBuilderFactory.newInstance
+    factory.setNamespaceAware(true)
+    factory.newDocumentBuilder.parse(new InputSource(new StringReader(body.trim())))
+  }
+}
